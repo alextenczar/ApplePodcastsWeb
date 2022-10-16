3c31df3cbdf4bd7187fb67fc44a3a3a8979b55a3
Alex Tenczar <atenczar@launchtheory.com>
2022-10-15 22:04:30 -0400

    console.log 2

--- middleware.ts
+++ middleware.ts
@@ -7,10 +7,10 @@ interface Info {
 
 export function middleware(request: NextRequest) {
   const response = NextResponse.next()
-  console.log(request.geo)
+  console.log(request.geo.country)
   let country = 'test'
   if (request?.geo?.country !== undefined && request?.geo?.country !== null) {
-      country = request?.geo?.country
+      country = request.geo.country
   }
   response.headers.set('X-Country', country);
   return response
