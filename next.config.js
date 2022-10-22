099e70c1c0cb6318e47bfab5eb4710ff5fd57517
Alex Tenczar <atenczar@launchtheory.com>
2022-10-22 12:20:05 -0400

    Initial Transition to Next.js Image Component

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+      remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.mzstatic.com',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
